0b56abaa9ad6ef1cf920431be8f91da9408abff3
SBence <43538202+SBence@users.noreply.github.com>
2021-05-03 00:07:10 +0200

    Move guild prefix to database

--- mikobot.js
+++ mikobot.js
@@ -33,11 +33,35 @@ const Guilds = database.define('guilds', {
 console.log('Syncing guild database...');
 Guilds.sync();
 
-bot.on('message', message => {
-    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+bot.on('message', async message => {
+    const guildEntry = await Guilds.findOne({ where: { id: message.guild.id } });
+    let prefix;
+
+    if (guildEntry) {
+        prefix = guildEntry.get('prefix');
+    } else {
+        try {
+            const newGuild = await Guilds.create({
+                id: message.guild.id
+            });
+            prefix = newGuild.get('prefix');
+            console.log(`New guild added to database\nName: ${message.guild.name}\nID: ${message.guild.id}`);
+        }
+        catch (e) {
+            message.reply('A database error occurred while trying to run your command.');
+            if (e.name === 'SequelizeUniqueConstraintError') {
+                return console.log(`A guild entry with a matching ID (${message.guild.id}) already exists in the database.
+                This error usually indicates a bug in the bot code, not in Sequelize.`);
+            }
+            return console.log(`An error has occurred adding the guild entry to the database with the following ID: ${message.guild.id}`);
+        }
+    }
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
     if (!bot.commands.has(command)) return;
+
     try {
         bot.commands.get(command).run(message, args);
     } catch (error) {
