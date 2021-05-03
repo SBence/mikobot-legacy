7f0155077aea0bc70f13fe816a4b7f16a9c86e17
SBence <43538202+SBence@users.noreply.github.com>
2021-05-03 03:46:03 +0200

    Move Guilds model to separate file

--- mikobot.js
+++ mikobot.js
@@ -11,24 +11,14 @@ const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'
 
 loadFiles(commandFiles, eventFiles);
 
-const database = new Sequelize('database', 'user', 'password', {
+const database = new Sequelize('database', 'username', 'password', {
     host: 'localhost',
     dialect: 'sqlite',
     logging: false,
     storage: 'config/database.sqlite'
 });
 
-const Guilds = database.define('guilds', {
-    id: {
-        type: Sequelize.STRING,
-        primaryKey: true
-    },
-    prefix: {
-        type: Sequelize.STRING,
-        defaultValue: 'm.',
-        allowNull: false
-    }
-});
+const Guilds = require('./models/Guilds')(database, Sequelize.DataTypes);
 
 console.log('Syncing guild database...');
 Guilds.sync();
@@ -45,13 +35,12 @@ bot.on('message', async message => {
                 id: message.guild.id
             });
             prefix = newGuild.get('prefix');
-            console.log(`New guild added to database\nName: ${message.guild.name}\nID: ${message.guild.id}`);
+            console.log(`[i]-----------------------------\nNew guild added to database\nName: ${message.guild.name}\nID: ${message.guild.id}\n[i]-----------------------------`);
         }
         catch (e) {
-            message.reply('A database error occurred while trying to run your command.');
+            message.reply('A database error has occurred while trying to run your command.');
             if (e.name === 'SequelizeUniqueConstraintError') {
-                return console.log(`A guild entry with a matching ID (${message.guild.id}) already exists in the database.
-                This error usually indicates a bug in the bot code, not in Sequelize.`);
+                return console.log(`A guild entry with a matching ID (${message.guild.id}) already exists in the database. This error usually indicates a bug in the bot code, not in Sequelize.`);
             }
             return console.log(`An error has occurred adding the guild entry to the database with the following ID: ${message.guild.id}`);
         }
@@ -63,7 +52,7 @@ bot.on('message', async message => {
     if (!bot.commands.has(command)) return;
 
     try {
-        bot.commands.get(command).run(message, args);
+        bot.commands.get(command).run(message, args, database, Sequelize.DataTypes);
     } catch (error) {
         console.error(error);
         message.reply('There was an error trying to run that command.');
---
+++ models/Guilds.js
@@ -0,0 +1,15 @@
+module.exports = (database, DataTypes) => {
+    return database.define('guilds', {
+        id: {
+            type: DataTypes.STRING,
+            primaryKey: true
+        },
+        prefix: {
+            type: DataTypes.STRING,
+            defaultValue: 'm.',
+            allowNull: false
+        }
+    }, {
+        timestamps: false
+    });
+};
