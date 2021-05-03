36ac34376771f85162f9fb9dd4ddf03077b8ede2
SBence <43538202+SBence@users.noreply.github.com>
2021-05-03 03:47:07 +0200

    Add setPrefix command

---
+++ commands/setPrefix.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'setprefix',
+    description: '',
+    async run(message, args, database, DataTypes) {
+        const Guilds = require('../models/Guilds')(database, DataTypes);
+        try {
+            const affectedRows = await Guilds.update({ prefix: args[0] }, { where: { id: message.guild.id } });
+            // TODO: Add feedback to user, trim and check for valid input.
+        }
+        catch (e) {
+            message.reply('An error has occurred while trying to change the prefix.');
+            return console.error(e);
+        }
+    }
+};
